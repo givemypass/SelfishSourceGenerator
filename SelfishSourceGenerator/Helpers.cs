ee2ff4ddd3f3fdec49c5b97183b3de24398c6de5
danila.golovenko <dgolovenko01@gmail.com>
2025-08-26 20:54:30 +0400

    upd: turn isourcegenerator into iincrementalgenerator

---
+++ SelfishSourceGenerator/Helpers.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace SelfishSourceGenerator
+{
+    public static class Helpers
+    {
+        public static bool IsDerivedFrom(this INamedTypeSymbol baseType, string targetType)
+        {
+            while (baseType != null)
+            {
+                if (baseType.Name == targetType)
+                    return true;
+
+                baseType = baseType.BaseType;
+            }
+
+            return false;
+        }
+
+        public static bool IsImplementingInterface(this ITypeSymbol type, string @interface)
+        {
+            foreach (var implemented in type.AllInterfaces)
+                if (implemented.Name == @interface)
+                    return true;
+
+            return false;
+        }
+    }
+}
